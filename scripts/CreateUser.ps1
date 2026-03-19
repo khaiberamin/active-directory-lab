cff46a8cb4ceff289e76e7aa0ee3cc962e9b2a2c
khaiberamin <148677277+khaiberamin@users.noreply.github.com>
2026-03-18 22:28:12 -0700

    added user creation scripts

---
+++ scripts/CreateUser.ps1
@@ -0,0 +1,31 @@
+﻿
+param(
+    [string]$FirstName,
+    [string]$LastName,
+    [string]$AccountName,
+    [string]$OU,
+    [string]$Domain = "homelab.local",
+    [string]$Password = "Password123"
+    )
+
+Import-Module ActiveDirectory -ErrorAction Stop
+
+$FirstHalfDomain = $Domain.Split('.')[0]
+$SecondHalfDomain = $Domain.Split('.')[1]
+
+New-ADUser `
+-Name "$FirstName $LastName" `
+-GivenName "$FirstName" `
+-Surname "$LastName" `
+-SamAccountName "$AccountName" `
+-UserPrincipalName "$AccountName@$Domain" `
+-Path "OU=$OU,DC=$FirstHalfDomain,DC=$SecondHalfDomain" `
+-AccountPassword (ConvertTo-SecureString "$Password" -AsPlainText -Force) `
+-ChangePasswordAtLogon $true `
+-Enabled $true
+
+$GroupName = "${OU}_Group"
+
+Add-ADGroupMember -Identity $GroupName -Members $AccountName
+
+#end of script ------------------------------
---
+++ scripts/CreateUserBulk.ps1
@@ -0,0 +1,36 @@
+﻿
+param(
+    [string]$CSVPath
+    )
+
+Import-Module ActiveDirectory -ErrorAction Stop
+
+$Users = Import-Csv -Path $CSVPath
+
+$Domain = "homelab.local"
+$FirstHalfDomain = $Domain.Split('.')[0]
+$SecondHalfDomain = $Domain.Split('.')[1]
+$Password = "Password123"
+
+foreach ($User in $Users) {
+   
+   New-ADUser `
+   -Name "$($User.FirstName) $($User.LastName)" `
+   -GivenName $User.FirstName `
+   -Surname $User.LastName `
+   -SamAccountName $User.AccountName `
+   -UserPrincipalName "$($User.AccountName)@$Domain" `
+   -Path "OU=$($User.OU),DC=$FirstHalfDomain,DC=$SecondHalfDomain" `
+   -AccountPassword (ConvertTo-SecureString "$Password" -AsPlainText -Force) `
+   -ChangePasswordAtLogon $true `
+   -Enabled $true
+
+   $GroupName = "$($User.OU)_Group"
+   Add-ADGroupMember -Identity $GroupName -Members $User.AccountName
+
+   Write-Host "Created User $($User.FirstName) $($User.LastName) in $($User.OU)"
+    
+}
+   
+
+#end of script ------------------------------
