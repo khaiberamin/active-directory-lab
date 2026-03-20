3567ca5ecb8ec4823fa28bea82a9f000a35506c5
khaiberamin <148677277+khaiberamin@users.noreply.github.com>
2026-03-20 02:01:36 -0700

    Add files via upload

---
+++ scripts/ReassignManager.ps1
@@ -0,0 +1,17 @@
+﻿param(
+    [Parameter(Mandatory=$true)]
+    [string]$OldManagerUsername,
+    
+    [Parameter(Mandatory=$true)]
+    [string]$NewManagerUsername
+)
+
+
+$oldManager = Get-ADUser -Identity $OldManagerUsername
+$newManager = Get-ADUser -Identity $NewManagerUsername
+
+Get-ADUser -Filter {Manager -eq $oldManager.DistinguishedName} |
+ForEach-Object {
+  Set-ADUser -Identity $_ -Manager $newManager.DistinguishedName
+  Write-Output "Changing Manager of user $($_.SamAccountName) from $OldManagerUsername to $NewManagerUsername"
+}
